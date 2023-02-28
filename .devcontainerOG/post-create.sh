3a31f9d0f0eee893abb391734c00d4f441174012
solarslurpi <happyday.mjohnson@gmail.com>
2023-02-28 12:39:15 +0000

    customized the devcontainer options.,

--- .devcontainerOG/post-create.sh
+++ .devcontainerOG/post-create.sh
@@ -3,7 +3,7 @@
 set -e # Fail the whole script on first error
 
 # Fetch Ruby gem dependencies
-bundle install --path vendor/bundle --with='development'
+bundle install --path vendor/bundle --with='development test'
 
 # Fetch Javascript dependencies
 yarn install
@@ -11,9 +11,11 @@ yarn install
 # Make Gemfile.lock pristine again
 git checkout -- Gemfile.lock
 
-# [re]create, migrate, and seed the developer database
-RAILS_ENV=development ./bin/rails db:setup
+# [re]create, migrate, and seed the test database
+RAILS_ENV=test ./bin/rails db:setup
 
 # Precompile assets for development
 RAILS_ENV=development ./bin/rails assets:precompile
 
+# Precompile assets for test
+RAILS_ENV=test NODE_ENV=tests ./bin/rails assets:precompile
